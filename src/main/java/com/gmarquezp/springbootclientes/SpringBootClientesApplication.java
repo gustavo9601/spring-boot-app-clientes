0d5012447224f17d0b07070b560b28a8f33cd933
gustavo9601 <tavo9601@gmail.com>
2022-07-20 15:38:11 -0500

    init project

---
+++ src/main/java/com/gmarquezp/springbootclientes/SpringBootClientesApplication.java
@@ -0,0 +1,13 @@
+package com.gmarquezp.springbootclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootClientesApplication.class, args);
+    }
+
+}
